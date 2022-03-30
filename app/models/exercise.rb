2b19964eef2de3a4fefc96aac889f98988956277
Heitor Cardozo <heitor711n@gmail.com>
2022-03-30 17:00:06 -0300

    Added exercise controller logic

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -2,7 +2,7 @@
 
 class Exercise < ApplicationRecord
   validates :description, :name, presence: true
-  validates :intensity, presence: true, inclusion: { in: 0..10 }
+  validates :intensity, presence: true, inclusion: { in: 0..10 }, numericality: { only_integer: true }
 
   has_many :exercise_workouts
   has_many :workouts, through: :exercise_workouts
