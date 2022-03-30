d042e5d7e4b58286786cc5055052056f6c465107
Heitor Cardozo <heitor711n@gmail.com>
2022-03-30 17:38:22 -0300

    Added workout controller logic

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -3,6 +3,6 @@
 class Workout < ApplicationRecord
   validates :name, presence: true
 
-  has_many :exercise_workouts
+  has_many :exercise_workouts, dependent: :delete_all
   has_many :exercises, through: :exercise_workouts
 end
