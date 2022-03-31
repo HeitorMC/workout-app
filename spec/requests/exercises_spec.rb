01351456e3df84c855dd58618dd9804dbf9f2eac
Heitor Cardozo <heitor711n@gmail.com>
2022-03-31 15:57:03 -0300

    Refactored tests of exercise controller

--- spec/requests/exercises_spec.rb
+++ spec/requests/exercises_spec.rb
@@ -4,57 +4,66 @@ require 'rails_helper'
 
 RSpec.describe '/exercises', type: :request do
   describe 'GET /index' do
-    let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-    let!(:auth) { sign_in(user) }
-    let!(:exercise) { create(:exercise) }
-
-    before { get exercises_path }
+    before do
+      create(:exercise)
+      user = create(:user, email: 'John_Doe@test.com', password: 'password')
+      sign_in(user)
+    end
 
     it 'renders a successful response' do
+      get exercises_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
-    let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-    let!(:auth) { sign_in(user) }
-    let!(:exercises) { create_list(:exercise, 5) }
+    before do
+      user = create(:user, email: 'John_Doe@test.com', password: 'password')
+      sign_in(user)
+    end
 
-    before { get exercise_path(exercises.last) }
+    let(:exercises) { create_list(:exercise, 5) }
 
     it 'renders a successful response' do
+      get exercise_path(exercises.last)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /new' do
-    let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-    let!(:auth) { sign_in(user) }
-
-    before { get new_exercise_path }
+    before do
+      user = create(:user, email: 'John_Doe@test.com', password: 'password')
+      sign_in(user)
+    end
 
     it 'renders a successful response' do
+      get new_exercise_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /edit' do
-    let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-    let!(:auth) { sign_in(user) }
-    let!(:exercise) { create_list(:exercise, 5) }
+    before do
+      user = create(:user, email: 'John_Doe@test.com', password: 'password')
+      sign_in(user)
+    end
 
-    before { get edit_exercise_path(exercise.last) }
+    let(:exercise) { create_list(:exercise, 5) }
 
     it 'renders a successful response' do
+      get edit_exercise_path(exercise.last)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'POST /create' do
     context 'when the parameters are valid' do
+      before do
+        user = create(:user, email: 'John_Doe@test.com', password: 'password')
+        sign_in(user)
+      end
+
       let(:valid_attributes) { { exercise: { name: 'Squat', description: 'lorem', intensity: 9 } } }
-      let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-      let!(:auth) { sign_in(user) }
 
       it 'creates a new Exercise' do
         expect { post exercises_path(valid_attributes) }.to change(Exercise, :count).by(1)
@@ -67,9 +76,12 @@ RSpec.describe '/exercises', type: :request do
     end
 
     context 'when the parameters are invalid' do
+      before do
+        user = create(:user, email: 'John_Doe@test.com', password: 'password')
+        sign_in(user)
+      end
+
       let(:invalid_attributes) { { exercise: { name: '', description: '', intensity: 'x' } } }
-      let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-      let!(:auth) { sign_in(user) }
 
       it 'does not create a new Exercise' do
         expect { post exercises_path(invalid_attributes) }.to change(Exercise, :count).by(0)
@@ -84,39 +96,47 @@ RSpec.describe '/exercises', type: :request do
 
   describe 'PATCH /update' do
     context 'when the parameters are valid' do
-      let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-      let!(:auth) { sign_in(user) }
+      before do
+        user = create(:user, email: 'John_Doe@test.com', password: 'password')
+        sign_in(user)
+      end
+
       let(:valid_attributes) { { exercise: { name: 'Squat', description: 'lorem', intensity: 9 } } }
       let(:basic_exercise) { create(:exercise) }
 
-      before { patch exercise_path(basic_exercise.id, valid_attributes) }
-
       it 'updates the requested exercise' do
+        patch exercise_path(basic_exercise.id, valid_attributes)
         expect(response).to have_http_status(:found)
       end
 
       it 'redirects to the exercise' do
+        patch exercise_path(basic_exercise.id, valid_attributes)
         expect(response).to redirect_to(exercise_path(basic_exercise.id))
       end
     end
 
     context 'when the parameters are invalid' do
+      before do
+        user = create(:user, email: 'John_Doe@test.com', password: 'password')
+        sign_in(user)
+      end
+
       let(:invalid_attributes) { { exercise: { name: '', description: '', intensity: 'x' } } }
-      let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-      let!(:auth) { sign_in(user) }
       let(:basic_exercise) { create(:exercise) }
 
-      before { patch exercise_path(basic_exercise.id, invalid_attributes) }
-
       it 'renders unprocessable entity response)' do
+        patch exercise_path(basic_exercise.id, invalid_attributes)
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
   end
 
   describe 'DELETE /destroy' do
-    let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
-    let!(:auth) { sign_in(user) }
+    before do
+      user = create(:user, email: 'John_Doe@test.com', password: 'password')
+      sign_in(user)
+    end
+
     let!(:basic_exercise) { create(:exercise) }
 
     it 'destroys the requested exercise' do
