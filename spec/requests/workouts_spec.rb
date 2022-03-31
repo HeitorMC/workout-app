6e046c19df937bb67a42c8657b59c93fea866aca
Heitor Cardozo <heitor711n@gmail.com>
2022-03-30 23:51:58 -0300

    Small fixes on workout controller

--- spec/requests/workouts_spec.rb
+++ spec/requests/workouts_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe '/workouts', type: :request do
     let!(:auth) { sign_in(user) }
     let!(:workout) { create(:workout) }
 
-    before { get workouts_url }
+    before { get workouts_path }
 
     it 'renders a successful response' do
       expect(response).to have_http_status(:success)
@@ -20,7 +20,7 @@ RSpec.describe '/workouts', type: :request do
     let!(:auth) { sign_in(user) }
     let!(:workouts) { create_list(:workout, 5) }
 
-    before { get workout_url(workouts.last.id) }
+    before { get workout_path(workouts.last) }
 
     it 'renders a successful response' do
       expect(response).to have_http_status(:success)
@@ -31,7 +31,7 @@ RSpec.describe '/workouts', type: :request do
     let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
     let!(:auth) { sign_in(user) }
 
-    before { get new_workout_url }
+    before { get new_workout_path }
 
     it 'renders a successful response' do
       expect(response).to have_http_status(:success)
@@ -41,9 +41,9 @@ RSpec.describe '/workouts', type: :request do
   describe 'GET /edit' do
     let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
     let!(:auth) { sign_in(user) }
-    let!(:workouts) { create_list(:exercise, 5) }
+    let!(:workouts) { create_list(:workout, 5) }
 
-    before { get edit_exercise_url(workouts.last.id) }
+    before { get edit_workout_path(workouts.last) }
 
     it 'renders a successful response' do
       expect(response).to have_http_status(:success)
@@ -53,30 +53,30 @@ RSpec.describe '/workouts', type: :request do
   describe 'POST /create' do
     context 'when the parameters are valid' do
       let(:valid_attributes) { { workout: { name: 'Euro Training' } } }
+      let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
       let!(:auth) { sign_in(user) }
-      let(:headers) { { Authorization: sign_in(user) } }
 
       it 'creates a new Workout' do
-        expect { post workouts_url(valid_attributes) }.to change(Workout, :count).by(1)
+        expect { post workouts_path(valid_attributes) }.to change(Workout, :count).by(1)
       end
 
       it 'redirects to the created workout' do
-        post workouts_url(valid_attributes)
-        expect(response).to redirect_to(workout_url(Workout.last))
+        post workouts_path(valid_attributes)
+        expect(response).to redirect_to(workout_path(Workout.last))
       end
     end
 
     context 'when the parameters are invalid' do
       let(:invalid_attributes) { { workout: { name: '' } } }
+      let(:user) { create(:user, email: 'John_Doe@test.com', password: 'password') }
       let!(:auth) { sign_in(user) }
-      let(:headers) { { Authorization: sign_in(user) } }
 
       it 'does not create a new Workout' do
-        expect { post workouts_url(invalid_attributes) }.to change(Workout, :count).by(0)
+        expect { post workouts_path(invalid_attributes) }.to change(Workout, :count).by(0)
       end
 
       it 'renders unprocessable entity response' do
-        post workouts_url(invalid_attributes)
+        post workouts_path(invalid_attributes)
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
@@ -89,14 +89,14 @@ RSpec.describe '/workouts', type: :request do
       let(:valid_attributes) { { workout: { name: 'Euro Training' } } }
       let(:workout) { create(:workout) }
 
-      before { patch workout_url(workout.id, valid_attributes) }
+      before { patch workout_path(workout.id, valid_attributes) }
 
       it 'updates the requested workout' do
         expect(response).to have_http_status(:found)
       end
 
       it 'redirects to the workout' do
-        expect(response).to redirect_to(workout_url(workout.id))
+        expect(response).to redirect_to(workout_path(workout.id))
       end
     end
 
@@ -106,7 +106,7 @@ RSpec.describe '/workouts', type: :request do
       let(:invalid_attributes) { { workout: { name: '' } } }
       let(:workout) { create(:workout) }
 
-      before { patch workout_url(workout.id, invalid_attributes) }
+      before { patch workout_path(workout.id, invalid_attributes) }
 
       it 'renders unprocessable entity response' do
         expect(response).to have_http_status(:unprocessable_entity)
@@ -121,12 +121,12 @@ RSpec.describe '/workouts', type: :request do
     let!(:workout) { create(:workout, exercise_ids: [exercise.first.id, exercise.last.id]) }
 
     it 'destroys the requested workout' do
-      expect { delete workout_url(workout.id) }.to change(Workout, :count).by(-1)
+      expect { delete workout_path(workout.id) }.to change(Workout, :count).by(-1)
     end
 
     it 'redirects to the workouts list' do
-      delete workout_url(workout.id)
-      expect(response).to redirect_to(workouts_url)
+      delete workout_path(workout.id)
+      expect(response).to redirect_to(workouts_path)
     end
   end
 end
